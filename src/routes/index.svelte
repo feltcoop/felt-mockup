666b0352a6168505d8a6ab609fe505dcd8c8adb5
Ryan Atkinson <ryanatkn@gmail.com>
2020-02-02 09:48:51 -0600

    improve styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,7 +8,12 @@
 	let notes = [
 		{ content: 'im a note :)' },
 		{
-			content: `<span class="text-4xl font-hairline">hi, <span class="text-6xl text-gray-600">welcome!</span> ty for stoppin by</span>`,
+			content: `
+				<span class="text-4xl font-hairline">
+					hi, <span class="text-6xl text-gray-600">welcome!</span>
+					ty for stopping by
+				</span>
+			`,
 		},
 	];
 
@@ -42,38 +47,44 @@
 			</li>
 		{/each}
 	</ul>
-	<Content>
-		<ol class="text-2xl" style="padding-left: 36px;">
-			<li class="font-hairline">
+	<ul class="text-2xl">
+		<li class="font-hairline">
+			<div>
 				this is a
 				<span class="font-medium bg-red-200">pre-alpha preview version</span>
 				of the software.
+			</div>
+			<div>
 				<span class="font-bold bg-red-400">
 					<span class="font-medium">expect nothing!</span>
 					~~assume security fails 100%~~
 				</span>
-			</li>
-			<li>
-				<!-- wrap it so it doesn't fill the space -->
-				<div class="flex items-center justify-center">
-					<a
-						href="/about"
-						class="logo-heart flex flex-wrap items-top justify-center mt-8">
-						<span class="text-6xl mr-2 font-hairline">about</span>
-						<img
-							src="/logo-heart.png"
-							alt="fuzzy felt heart"
-							style="width: 192px; height: 178px; margin-top: -13px" />
-					</a>
-				</div>
-			</li>
-			<li>
-				<span class="font-hairline">
+			</div>
+		</li>
+		<li>
+			<!-- wrap it so it doesn't fill the space -->
+			<div class="flex items-center justify-center">
+				<a
+					href="/about"
+					class="logo-heart flex flex-wrap items-top justify-center mt-8">
+					<span class="text-6xl mr-2 pb-2 font-hairline text-green-700">
+						about
+					</span>
+					<img
+						src="/logo-heart.png"
+						alt="fuzzy felt heart"
+						style="width: 192px; height: 178px; margin-top: -18px" />
+				</a>
+			</div>
+		</li>
+		<li class="mb-8">
+			<Content classes="font-hairline text-center mb-4">
+				<div>
 					<a
 						href="https://en.wikipedia.org/wiki/Free_software"
 						target="_blank"
 						rel="noopener"
-						class="text-4xl font-hairline">
+						class="exclaim">
 						!!
 					</a>
 					free
@@ -81,29 +92,40 @@
 						href="https://en.wikipedia.org/wiki/Free_and_open-source_software"
 						target="_blank"
 						rel="noopener"
-						class="text-4xl font-hairline">
+						class="exclaim">
 						!!
 					</a>
-					source code at
-				</span>
+					source code
+				</div>
+				at
 				<a
+					class="github-repo"
 					href="https://github.com/feltcoop/felt"
 					target="_blank"
 					rel="noopener">
 					github.com/feltcoop/felt
 				</a>
-			</li>
-			<li>
-				<SocialLinks />
-			</li>
-		</ol>
-	</Content>
+			</Content>
+		</li>
+		<li>
+			<SocialLinks />
+		</li>
+	</ul>
 </div>
 
 <style>
+	.github-repo {
+		@apply font-normal;
+	}
+	.exclaim {
+		@apply text-6xl font-hairline text-gray-600;
+	}
 	.logo-heart img {
 		transform: scale3d(1, 1, 1);
 		transition: transform 0.13s ease-out;
+	}
+	.logo-heart:hover {
+		text-decoration: underline;
 	}
 	.logo-heart:hover img {
 		transform: scale3d(1.03, 1.03, 1);
