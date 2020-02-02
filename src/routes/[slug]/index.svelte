8276ae4f2f270451aa844e88b6b83b20dc6e1067
Ryan Atkinson <ryanatkn@gmail.com>
2020-02-02 03:20:10 -0600

    fix world input box

--- src/routes/[slug]/index.svelte
+++ src/routes/[slug]/index.svelte
@@ -7,6 +7,16 @@
 	const world = useWorld();
 
 	let text = '';
+
+	const defaultNotes = () => [{ content: 'im a note :)' }];
+	let notes = defaultNotes();
+
+	$: $world, (notes = defaultNotes()); // TODO this is a hack to make things less confusing - the fix is to persist state per world
+
+	const submit = () => {
+		notes = [...notes, { content: text }];
+		text = '';
+	};
 </script>
 
 <svelte:head>
@@ -16,10 +26,19 @@
 <World {world} />
 
 <div class="flex flex-col p-2">
-	{#if text}
-		<div class="border-4 border-purple-200 rounded-bl-lg rounded-tr-lg">
-			<div class="px-2">{text}</div>
-		</div>
-	{/if}
-	<TextInput bind:value={text} />
+	<ul>
+		{#each notes as note}
+			<li>
+				{@html note.content}
+			</li>
+		{/each}
+		{#if text}
+			<li class="border-4 border-purple-200 rounded-bl-lg rounded-tr-lg">
+				<div class="px-2">
+					{@html text}
+				</div>
+			</li>
+		{/if}
+	</ul>
+	<TextInput bind:value={text} {submit} />
 </div>
