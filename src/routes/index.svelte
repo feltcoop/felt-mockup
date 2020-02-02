9d5f4a84c93241fa1541a23599b84ae3c8121afc
Ryan Atkinson <ryanatkn@gmail.com>
2020-02-02 04:35:27 -0600

    improve the intro screen

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,17 @@
 <script>
 	import Content from '@app/ui/Content.svelte';
 	import TextInput from '@app/ui/TextInput.svelte';
+	import SocialLinks from '@app/ui/SocialLinks.svelte';
 
 	let text = '';
 
-	let notes = [{ content: 'im a note :)' }];
+	let notes = [
+		{ content: 'im a note :)' },
+		{
+			content:
+				'hi, <span class="font-bold"><span class="text-purple-700">w</span><span class="text-green-700">e</span><span class="text-purple-700">l</span><span class="text-green-700">c</span><span class="text-purple-700">o</span><span class="text-green-700">m</span><span class="text-purple-700">e</span><span class="text-green-700">!</span><span class="text-purple-700">!</span></span> ty for stoppin by',
+		},
+	];
 
 	const submit = () => {
 		notes = [{ content: text }, ...notes];
@@ -36,4 +43,28 @@
 			</li>
 		{/each}
 	</ul>
+	<Content>
+		<ol class="text-2xl" style="padding-left: 36px;">
+			<li>
+				<a href="/about" class="flex">
+					<span class="text-4xl mr-4">about</span>
+					<img src="/logo-heart.png" alt="fuzzy felt heart" />
+				</a>
+			</li>
+			<li>
+				free
+				<small>!!!</small>
+				source code at
+				<a
+					href="https://github.com/feltcoop/felt"
+					target="_blank"
+					rel="noopener">
+					github.com/feltcoop/felt
+				</a>
+			</li>
+			<li>
+				<SocialLinks />
+			</li>
+		</ol>
+	</Content>
 </div>
