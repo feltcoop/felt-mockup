6890bdf275f37b84ba63598506d2f6461da1264f
Ryan Atkinson <ryanatkn@gmail.com>
2020-02-07 03:43:23 -0600

    chonky button outline style

--- src/app/ui/Button.svelte
+++ src/app/ui/Button.svelte
@@ -4,9 +4,11 @@
 	export let disabled;
 </script>
 
+<!-- TODO custom colors -->
 <button
 	class="border-2 border-green-500 rounded-tl-sm rounded-br-sm rounded-bl-lg
-	rounded-tr-lg text-primary font-bold px-3 pt-2 pb-3 relative hover:underline {classes}"
+	rounded-tr-lg text-primary font-bold px-3 pt-2 pb-3 relative hover:underline
+	focus:underline {classes}"
 	{disabled}
 	type="button"
 	on:click
@@ -17,9 +19,15 @@
 <style lang="postcss">
 	button {
 		box-shadow: 1px -1px 0 1px rgba(84, 153, 73, 0.75) inset;
+		outline-width: 4px;
 	}
 	button:active {
 		box-shadow: 0 1px 0 1px rgba(84, 153, 73, 0.75) inset;
 		top: 1px;
+	}
+	button:focus {
+		outline-width: 3px;
+		outline-style: dotted;
+		outline-color: rgba(84, 153, 73, 0.75);
 	}
 </style>
