d1159ed16d6cf17093a0e04720b3a6be1f490a02
Ryan Atkinson <ryanatkn@gmail.com>
2020-02-08 23:55:28 -0600

    soften button colors

--- src/app/ui/Button.svelte
+++ src/app/ui/Button.svelte
@@ -6,8 +6,8 @@
 
 <!-- TODO custom colors -->
 <button
-	class="border-2 border-green-500 rounded-tl-sm rounded-br-sm rounded-bl-lg
-	rounded-tr-lg text-primary font-bold px-3 pt-2 pb-3 relative hover:underline
+	class="border-2 border-green-300 rounded-tl-sm rounded-br-sm rounded-bl-lg
+	rounded-tr-lg text-green-500 font-bold px-3 pt-2 pb-3 relative hover:underline
 	focus:underline {classes}"
 	{disabled}
 	type="button"
@@ -18,13 +18,13 @@
 
 <style>
 	button {
-		box-shadow: 1px -1px 0 1px rgba(84, 153, 73, 0.75) inset;
+		box-shadow: 1px -1px 0 1px #9ae6b4 inset;
 	}
 	button:active {
-		box-shadow: 0 1px 0 1px rgba(84, 153, 73, 0.75) inset;
+		box-shadow: 0 1px 0 1px #9ae6b4 inset;
 		top: 2px;
 	}
 	button:focus {
-		outline: 4px dotted rgba(84, 153, 73, 0.75);
+		outline: 4px dotted #9ae6b4;
 	}
 </style>
