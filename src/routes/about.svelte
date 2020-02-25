a14fd8ab477b0561d80b48217561b3a68bd7624e
Ryan Atkinson <ryanatkn@gmail.com>
2020-02-25 15:44:01 -0600

    link worker-owners to twitter

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -51,7 +51,9 @@
 			And Felt is more than a big bag of features. It's a customizable
 			<a href="https://github.com/feltcoop/felt">
 				open-source software platform</a>,
-			<a href="https://felt.social">a community</a>, and a co-op of worker-owners, all with a purpose that puts people first.
+			<a href="https://felt.social">a community</a>,
+			and <a href="https://twitter.com/feltcoop">a co-op of worker-owners</a>,
+			all with a purpose that puts people first.
 			If we do it right, Felt is tech that feels good.
 		</p>
 		<p>
