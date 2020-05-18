c3789977e01ea5b7e8641c539ff5c05b1b99be63
Ryan Atkinson <ryanatkn@gmail.com>
2020-05-18 14:07:19 -0500

    add gro build task

---
+++ src/build.task.ts
@@ -0,0 +1,28 @@
+import {Task} from '@feltcoop/gro';
+import {spawnProcess} from '@feltcoop/gro/dist/utils/process.js';
+
+import {task as devTask} from './dev.task.js';
+
+export const task: Task = {
+	description: 'builds everything for production',
+	run: async (ctx): Promise<void> => {
+		const {log} = ctx;
+
+		log.info('compiling typescript');
+		await spawnProcess('node_modules/.bin/tsc');
+
+		await devTask.run({...ctx, args: {...ctx.args, watch: false}});
+
+		log.info('building sapper');
+		await spawnProcess('node_modules/.bin/sapper', [
+			'build',
+			'--legacy',
+			'--src',
+			'build',
+			'--routes',
+			'build/routes',
+			'--output',
+			'build/node_modules/@sapper',
+		]);
+	},
+};
