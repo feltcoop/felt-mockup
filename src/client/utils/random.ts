48fcdbaace0b07ebf31078128de6a9d718dd3f2a
Ryan Atkinson <ryanatkn@gmail.com>
2020-05-18 12:37:36 -0500

    import random utils from gro and remove local versions

--- src/client/utils/random.ts
+++
@@ -1,6 +0,0 @@
-// TODO consider checking if max < min, because it doesn't work correctly
-export const randInt = (min: number, max: number): number =>
-	Math.floor(Math.random() * (max - min + 1)) + min;
-
-export const randItem = <T>(arr: T[]): T | undefined =>
-	arr[randInt(0, arr.length - 1)];
