41c76f0d92a1a780ec699afe64d6bad48df919b2
Ryan Atkinson <ryanatkn@gmail.com>
2020-05-19 18:34:15 -0500

    remove legacy support and babel deps (#19)

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import resolve from 'rollup-plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import commonjs from 'rollup-plugin-commonjs';
 import svelte from 'rollup-plugin-svelte';
-import babel from 'rollup-plugin-babel';
 import {terser} from 'rollup-plugin-terser';
 import config from 'sapper/config/rollup.js';
 import {builtinModules} from 'module';
@@ -39,30 +38,6 @@ export default {
 				dedupe,
 			}),
 			commonjs(),
-
-			legacy &&
-				babel({
-					extensions: ['.js', '.html', '.svelte'],
-					runtimeHelpers: true,
-					exclude: ['node_modules/@babel/**'],
-					presets: [
-						[
-							'@babel/preset-env',
-							{
-								targets: '> 0.25%, not dead',
-							},
-						],
-					],
-					plugins: [
-						'@babel/plugin-syntax-dynamic-import',
-						[
-							'@babel/plugin-transform-runtime',
-							{
-								useESModules: true,
-							},
-						],
-					],
-				}),
 
 			!dev &&
 				terser({
