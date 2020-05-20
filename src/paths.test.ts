b200a7c83838cf30522e437c3c517710628fbcde
Ryan Atkinson <ryanatkn@gmail.com>
2020-05-19 19:08:22 -0500

    test that ci fails

--- src/paths.test.ts
+++ src/paths.test.ts
@@ -3,5 +3,5 @@ import {test, t} from '@feltcoop/gro';
 import {paths} from './paths.js';
 
 test('paths', () => {
-	t.ok(paths.db.startsWith(paths.source));
+	t.ok(!paths.db.startsWith(paths.source));
 });
