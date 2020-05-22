dd039ddbb123267bbd52d33046a7ac4d79001cae
Ryan Atkinson <ryanatkn@gmail.com>
2020-05-22 15:53:36 -0500

    reverse the check to avoid negation

--- src/setup.task.ts
+++ src/setup.task.ts
@@ -4,11 +4,11 @@ import {copy, pathExists} from '@feltcoop/gro/dist/fs/nodeFs.js';
 export const task: Task = {
 	description: 'performs initial project setup',
 	run: async ({log}): Promise<void> => {
-		if (!(await pathExists('.env'))) {
+		if (await pathExists('.env')) {
+			log.info('.env already exists - skipping its copy step');
+		} else {
 			log.info('copying initial .env to root');
 			await copy('src/project/setup/.env.sample', '.env');
-		} else {
-			log.info('.env already exists - skipping its copy step');
 		}
 	},
 };
