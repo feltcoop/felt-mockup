98263d642fec8274ce955320ed27b15cbe4e237f
Ryan Atkinson <ryanatkn@gmail.com>
2020-05-22 15:06:01 -0500

    add setup task

---
+++ src/setup.task.ts
@@ -0,0 +1,14 @@
+import {Task} from '@feltcoop/gro';
+import {copy, pathExists} from '@feltcoop/gro/dist/fs/nodeFs.js';
+
+export const task: Task = {
+	description: 'performs initial project setup',
+	run: async ({log}): Promise<void> => {
+		if (!(await pathExists('.env'))) {
+			log.info('copying initial .env to root');
+			await copy('src/project/setup/.env.sample', '.env');
+		} else {
+			log.info('.env already exists - skipping its copy step');
+		}
+	},
+};
