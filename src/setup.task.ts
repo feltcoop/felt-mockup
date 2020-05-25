e1946187d697b10274440cf57157d8abb11ea8b9
Ryan Atkinson <ryanatkn@gmail.com>
2020-05-25 18:33:42 -0500

    use the felt_dev db

--- src/setup.task.ts
+++ src/setup.task.ts
@@ -1,5 +1,7 @@
+import dotenv from 'dotenv';
 import {Task} from '@feltcoop/gro';
 import {copy, pathExists} from '@feltcoop/gro/dist/fs/nodeFs.js';
+import {spawnProcess} from '@feltcoop/gro/dist/utils/process.js';
 
 export const task: Task = {
 	description: 'performs initial project setup',
@@ -10,5 +12,32 @@ export const task: Task = {
 			log.info('copying initial .env to root');
 			await copy('src/project/setup/.env.sample', '.env');
 		}
+
+		dotenv.config();
+		const {DB_NAME, DB_USER, DB_PASS} = process.env;
+		if (!DB_NAME) throw Error(`Expected environment variable DB_NAME`);
+		if (!DB_USER) throw Error(`Expected environment variable DB_USER`);
+		if (!DB_PASS) throw Error(`Expected environment variable DB_PASS`);
+		process.env.PGPASSWORD = DB_PASS;
+		const spawnOptions = {env: process.env};
+		await spawnProcess(
+			`psql`,
+			['-c', `CREATE DATABASE ${DB_NAME};`, '-U', DB_USER, '-w'],
+			spawnOptions,
+		);
+		// TODO what's the right way to do this?
+		// there's currently a contradiction about setting up the user and password
+		// what does a fresh setup with no existing database look like?
+		// maybe we need to split the admin db user/password from the app user/password?
+		await spawnProcess(
+			`psql`,
+			['-c', `CREATE USER ${DB_USER} WITH PASSWORD '${DB_PASS}';`, '-U', DB_USER, '-w'],
+			spawnOptions,
+		);
+		await spawnProcess(
+			`psql`,
+			['-c', `ALTER ROLE ${DB_USER} WITH PASSWORD '${DB_PASS}';`, '-U', DB_USER, '-w'],
+			spawnOptions,
+		);
 	},
 };
