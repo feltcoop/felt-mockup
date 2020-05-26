45a21e0c02fec12468570d5d8cf020bb3ea5750b
Ryan Atkinson <ryanatkn@gmail.com>
2020-05-26 02:47:11 -0500

    attempt to run setup task in CI

--- src/setup.task.ts
+++ src/setup.task.ts
@@ -13,32 +13,35 @@ export const task: Task = {
 			await copy('src/project/setup/.env.sample', '.env');
 		}
 
-		if (!process.env.CI) {
-			dotenv.config();
-			const {DB_NAME, DB_USER, DB_PASS} = process.env;
-			if (!DB_NAME) throw Error(`Expected environment variable DB_NAME`);
-			if (!DB_USER) throw Error(`Expected environment variable DB_USER`);
-			if (!DB_PASS) throw Error(`Expected environment variable DB_PASS`);
-			process.env.PGPASSWORD = DB_PASS;
-			await spawnProcess(`psql`, ['-c', `CREATE DATABASE ${DB_NAME};`, '-U', DB_USER, '-w']);
-			// TODO what's the right way to do this?
-			// there's currently a contradiction about setting up the user and password
-			// what does a fresh setup with no existing database look like?
-			// maybe we need to split the admin db user/password from the app user/password?
-			await spawnProcess(`psql`, [
-				'-c',
-				`CREATE USER ${DB_USER} WITH PASSWORD '${DB_PASS}';`,
-				'-U',
-				DB_USER,
-				'-w',
-			]);
-			await spawnProcess(`psql`, [
-				'-c',
-				`ALTER ROLE ${DB_USER} WITH PASSWORD '${DB_PASS}';`,
-				'-U',
-				DB_USER,
-				'-w',
-			]);
-		}
+		dotenv.config();
+		const {DB_NAME, DB_USER, DB_PASS} = process.env;
+		if (!DB_NAME) throw Error(`Expected environment variable DB_NAME`);
+		if (!DB_USER) throw Error(`Expected environment variable DB_USER`);
+		if (!DB_PASS) throw Error(`Expected environment variable DB_PASS`);
+		process.env.PGPASSWORD = DB_PASS;
+		process.env.POSTGRES_HOST = 'localhost';
+		process.env.POSTGRES_PORT = '5432';
+		process.env.POSTGRES_DB = DB_NAME;
+		process.env.POSTGRES_USER = DB_USER;
+		process.env.POSTGRES_PASSWORD = DB_PASS;
+		await spawnProcess(`psql`, ['-c', `CREATE DATABASE ${DB_NAME};`, '-U', DB_USER, '-w']);
+		// TODO what's the right way to do this?
+		// there's currently a contradiction about setting up the user and password
+		// what does a fresh setup with no existing database look like?
+		// maybe we need to split the admin db user/password from the app user/password?
+		await spawnProcess(`psql`, [
+			'-c',
+			`CREATE USER ${DB_USER} WITH PASSWORD '${DB_PASS}';`,
+			'-U',
+			DB_USER,
+			'-w',
+		]);
+		await spawnProcess(`psql`, [
+			'-c',
+			`ALTER ROLE ${DB_USER} WITH PASSWORD '${DB_PASS}';`,
+			'-U',
+			DB_USER,
+			'-w',
+		]);
 	},
 };
