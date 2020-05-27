1e77f9e0f298d50d055026cf8d3f8c6c36ea78d8
Ryan Atkinson <ryanatkn@gmail.com>
2020-05-26 19:35:49 -0500

    remove comments

--- src/accounts/AccountModel.ts
+++ src/accounts/AccountModel.ts
@@ -4,5 +4,3 @@ export interface AccountModel {
 	id: ModelId;
 	email: string;
 }
-
-// TODO see `src/db/Model.ts` for discussion about what the right abstraction is for "models"
--- src/db/Model.ts
+++ src/db/Model.ts
@@ -1,10 +1,1 @@
-// TODO do we want to use this pattern for improved type safety?
-// reference: https://spin.atomicobject.com/2018/01/15/typescript-flexible-nominal-typing/
-// export type ModelId = Flavor<number, 'ModelId'>; // with helper
 export type ModelId = number & {__type?: 'ModelId'}; // incrementing integers
-
-// TODO abstract model class, similar to `./Repo.ts`?
-// or are models plain JSON objects with associated (and generally pure) function helpers?
-// if the latter, this file should probably be `model.ts` not `Model.ts`
-// right now I'm leaning more towards the decoupled, non-class version of a "model",
-// but perhaps we want both `Model` classes and a "record/doc/etc" plain JSON type that they wrap.
