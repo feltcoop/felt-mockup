f3c2ed4e3700b2732a0f88a141325a92e41bf2ee
Ryan Atkinson <ryanatkn@gmail.com>
2020-06-29 15:26:19 -0500

    fix a type

--- src/db/Model.ts
+++ src/db/Model.ts
@@ -1,5 +1,5 @@
 declare const ModelIdFlavorSymbol: unique symbol;
 
 export type ModelId<T extends string = string> = Flavored<number, 'ModelId'> & {
-	[ModelIdFlavorSymbol]: T;
+	[ModelIdFlavorSymbol]?: T;
 };
