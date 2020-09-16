56775d8ddf70a1b1f83367b5064ed5bdc06f2359
Ryan Atkinson <ryanatkn@gmail.com>
2020-09-16 01:04:27 -0500

    improve persona test cases

--- src/personas/PersonasRepo.test.ts
+++ src/personas/PersonasRepo.test.ts
@@ -7,51 +7,51 @@ import {PersonaModel} from './PersonaModel.js';
 const randomName = () => `persona_name_${Math.random()}`; // TODO clear the db each run?;
 
 test('PersonasRepo', () => {
-	const testAccount = 1; // TODO improve this? pretty hacky but fine for now. global setup to get some test accounts?
+	const testAccount1 = 1; // TODO improve this? pretty hacky but fine for now. global setup to get some test accounts?
 	const testAccount2 = 2;
 	const personasRepo = new PersonasRepo(getKnex());
-	const testName = randomName();
-	const testNameUppercase = testName.toUpperCase();
-	t.isNot(testName, testNameUppercase); // just in case something changes
-	let testPersona: PersonaModel;
+	const testName1a = randomName();
+	const testName1b = randomName();
+	const testName2a = randomName();
+	const testName2b = randomName();
+	const testName1aUppercase = testName1a.toUpperCase();
+	t.isNot(testName1a, testName1aUppercase); // just in case something changes
+	let testPersona1a: PersonaModel;
 
 	test('create()', async () => {
 		test('creates a persona with a case-insensitive name', async () => {
-			const result = await personasRepo.create({account: testAccount, name: testNameUppercase});
+			const result = await personasRepo.create({account: testAccount1, name: testName1aUppercase});
 			t.ok(result.ok);
-			t.is(result.value.name, testName);
-			testPersona = result.value;
+			t.is(result.value.name, testName1a);
+			testPersona1a = result.value;
 		});
 		test('creates a second persona for the same account', async () => {
-			const name = randomName();
-			const result = await personasRepo.create({account: testAccount, name});
+			const result = await personasRepo.create({account: testAccount1, name: testName1b});
 			t.ok(result.ok);
-			t.is(result.value.name, name);
+			t.is(result.value.name, testName1b);
 		});
 		test('creates a persona for a second account', async () => {
-			const name = randomName();
-			const result = await personasRepo.create({account: testAccount2, name});
+			const result = await personasRepo.create({account: testAccount2, name: testName2a});
 			t.ok(result.ok);
-			t.is(result.value.name, name);
+			t.is(result.value.name, testName2a);
 		});
 		test('creates a second persona for the second account', async () => {
-			const name = randomName();
-			const result = await personasRepo.create({account: testAccount2, name});
+			const result = await personasRepo.create({account: testAccount2, name: testName2b});
 			t.ok(result.ok);
-			t.is(result.value.name, name);
+			t.is(result.value.name, testName2b);
 		});
 		test('fails to create a persona with a duplicate name', async () => {
-			const result = await personasRepo.create({account: testAccount, name: testName});
+			const result = await personasRepo.create({account: testAccount1, name: testName1a});
 			t.ok(!result.ok);
 			t.is(result.type, 'duplicateName');
 		});
 		test('fails to create a persona with a duplicate name with different case', async () => {
-			const result = await personasRepo.create({account: testAccount, name: testNameUppercase});
+			const result = await personasRepo.create({account: testAccount1, name: testName1aUppercase});
 			t.ok(!result.ok);
 			t.is(result.type, 'duplicateName');
 		});
 		test('fails to create a persona with an invalid name', async () => {
-			const result = await personasRepo.create({account: testAccount, name: ''});
+			const result = await personasRepo.create({account: testAccount1, name: ''});
 			t.ok(!result.ok);
 			t.is(result.type, 'invalidName');
 		});
@@ -62,9 +62,9 @@ test('PersonasRepo', () => {
 
 	test('findById()', async () => {
 		test('finds one by id', async () => {
-			const result = await personasRepo.findById(testPersona.id);
+			const result = await personasRepo.findById(testPersona1a.id);
 			t.ok(result.ok);
-			t.equal(result.value, testPersona);
+			t.equal(result.value, testPersona1a);
 		});
 		test('fails to find a nonexistent persona', async () => {
 			const result = await personasRepo.findById(-1);
@@ -75,22 +75,22 @@ test('PersonasRepo', () => {
 
 	test('findByAccountAndName()', async () => {
 		test('finds one by account and name', async () => {
-			const result = await personasRepo.findByAccountAndName(testAccount, testName);
+			const result = await personasRepo.findByAccountAndName(testAccount1, testName1a);
 			t.ok(result.ok);
-			t.equal(result.value, testPersona);
+			t.equal(result.value, testPersona1a);
 		});
 		test('finds one by account and name with a different case', async () => {
-			const result = await personasRepo.findByAccountAndName(testAccount, testNameUppercase);
+			const result = await personasRepo.findByAccountAndName(testAccount1, testName1aUppercase);
 			t.ok(result.ok);
-			t.equal(result.value, testPersona);
+			t.equal(result.value, testPersona1a);
 		});
 		test('fails to find a nonexistent persona', async () => {
-			const result = await personasRepo.findByAccountAndName(testAccount, randomName());
+			const result = await personasRepo.findByAccountAndName(testAccount1, randomName());
 			t.ok(!result.ok);
 			t.is(result.type, 'noPersonaFound');
 		});
 		test('fails with an invalid name', async () => {
-			const result = await personasRepo.findByAccountAndName(testAccount, '');
+			const result = await personasRepo.findByAccountAndName(testAccount1, '');
 			t.ok(!result.ok);
 			t.is(result.type, 'invalidName');
 		});
@@ -98,19 +98,28 @@ test('PersonasRepo', () => {
 
 	test('findByAccount()', async () => {
 		test('finds all by account', async () => {
-			const result = await personasRepo.findByAccount(testAccount);
+			const result = await personasRepo.findByAccount(testAccount1);
 			t.ok(result.ok);
 			t.ok(result.value.length > 1);
 			for (const persona of result.value) {
-				t.is(persona.account, testAccount);
+				t.is(persona.account, testAccount1);
 			}
-			// query the second test account's personas
-			const result2 = await personasRepo.findByAccount(testAccount2);
-			t.ok(result2.ok);
-			t.ok(result2.value.length > 1);
-			for (const persona of result2.value) {
+			// TODO we probably want to create a new account every test run and
+			// ensure exactly these 2 persona names are found and no more
+			t.ok(result.value.find((p) => p.name === testName1a));
+			t.ok(result.value.find((p) => p.name === testName1b));
+		});
+		test('finds all for the second account', async () => {
+			const result = await personasRepo.findByAccount(testAccount2);
+			t.ok(result.ok);
+			t.ok(result.value.length > 1);
+			for (const persona of result.value) {
 				t.is(persona.account, testAccount2);
 			}
+			// TODO we probably want to create a new account every test run and
+			// ensure exactly these 2 persona names are found and no more
+			t.ok(result.value.find((p) => p.name === testName2a));
+			t.ok(result.value.find((p) => p.name === testName2b));
 		});
 		test('fails to find with an invalid account', async () => {
 			const result = await personasRepo.findByAccount(-1);
